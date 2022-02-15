8672b79b3a5ce9c0aed1bb78b570846fe6794bdf
Logan <lsaunders275@gmail.com>
2022-02-14 22:28:28 -0700

    fix tailwind niche

--- src/pages/Solver/Form/Input/Input.jsx
+++ src/pages/Solver/Form/Input/Input.jsx
@@ -1,6 +1,6 @@
 import { Children } from 'react'
 import { Controller } from 'react-hook-form'
-import { WORD_LENGTH } from '../../../CONSTANTS'
+// import { WORD_LENGTH } from '../../../CONSTANTS'
 
 const Input = ({
   label, caption, name, control, type = 'text', placeholder,
@@ -16,12 +16,16 @@ const Input = ({
   </div>
 )
 
+// tailwind needs to have the complete rule in a template literal;
+//    partials will get removed from the prod build
+//    https://tailwindcss.com/docs/content-configuration#class-detection-in-depth
+// const gridColumns = `grid-cols-${WORD_LENGTH}`
 const MultiInput = ({
   label, caption, names, control, type = 'text', numInputs,
 }) => (
   <div className="flex flex-col font-inter gap-1 mb-6">
     <label htmlFor="REPLACE_ME" className="font-bold">{label}</label>
-    <div className={`grid gap-0.5 grid-cols-${WORD_LENGTH}`}>
+    <div className="grid gap-0.5 grid-cols-5">
       {Children.toArray([...new Array(numInputs)].map((_, i) => (
         <Controller
           name={names[i]}
